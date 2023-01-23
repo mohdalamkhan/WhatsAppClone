987cb512b724cdf5da21ec7b8fd4281a5ae38f90
APPLE <apple@APPLEs-MacBook-Air.local>
2023-01-23 18:20:13 +0530

    UI-2 Chat List Item

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChatListItem from './src/components/ChatListItem';
+
+const chat ={
+  id:"1",
+  user:{
+    image:
+    'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg',
+    name: "Lucy",
+  },
+  lastMessage:{
+    text: "Hello",
+    createAt: "07:31",
+  },
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ChatListItem  chat ={chat}/>
+      {/* <ChatListItem /> */}
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/ChatListItem/index.js
@@ -0,0 +1,65 @@
+import { View, Text, StyleSheet,Image } from 'react-native'
+import React from 'react'
+
+const ChatListItem = ({chat}) => {
+  // console.log(props)
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: chat.user.image}}
+
+        style={styles.image}
+        />
+          <View style={styles.content}>
+              <View style={styles.row}>
+                  <Text numberOfLines={1} style={styles.name}>{chat.user.name}</Text>
+                      <Text style={styles.subTitle}>{chat.lastMessage.createAt}</Text>   
+              </View>
+              <Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage.text}</Text>
+          </View>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flexDirection:'row',
+    marginHorizontal:10,
+    marginVertical:5,
+    height:70,
+    // backgroundColor:'red',
+  },
+  image:{
+  width:60,
+  height:60,
+  borderRadius:30,
+  marginRight:10,
+  
+  // height: 70,
+  },
+  row:{
+    flexDirection:'row',
+    marginBottom:5,
+  },
+  content:{
+    flex: 1,
+
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'lightgray',
+    
+  },
+  name:{
+    flex: 1,
+    fontWeight:'bold',
+  },
+  subTitle:{
+    color:'gray',
+    marginBottom:2,
+  },
+
+
+
+
+});
+
+export default ChatListItem
